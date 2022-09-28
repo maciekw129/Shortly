6d23f526c77ea2961a98691d1b4991c0fc3ddaa0
Maciej Walecki <maciejwalecki99@gmail.com>
2022-09-28 11:08:43 +0200

    Configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,8 @@ module.exports = {
     output : {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name][contenthash].js',
-        clean: true
+        clean: true,
+        assetModuleFilename: '[name][ext]'
     },
     devServer: {
         static: {
@@ -32,6 +33,10 @@ module.exports = {
                         presets: ['@babel/preset-env']
                     }
                 }
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             }
         ],
     },
