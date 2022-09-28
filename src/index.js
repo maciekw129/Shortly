4be94090da0c4f8c1ce0b13c3c693992d5493732
Maciej Walecki <maciejwalecki99@gmail.com>
2022-09-28 11:12:45 +0200

    created logic js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-console.log(123);
-import './styles/main.scss';
+import './styles/style.scss';
+
+import illustrationWorking from './images/illustration-working.svg';
+import brandRecognitionIcon from './images/icon-brand-recognition.svg';
+import detailedRecordsIcon from './images/icon-detailed-records.svg';
+import fullyCustomizableIcon from './images/icon-fully-customizable.svg';
+
+import axios from 'axios';
+import createLinkElement from './helpers/createLinkElement';
+
+const mainButton = document.querySelector('.main__button');
+const mainInput = document.querySelector('.main__input');
+
+const form = document.querySelector('.main__input-wrapper');
+form.onsubmit = formSubmit;
+
+function formSubmit(event) {
+    event.preventDefault();
+    const link = document.querySelector('.main__input');
+    axios.post(`https://api.shrtco.de/v2/shorten?url=${link.value}`)
+    .then(response => {
+        createLinkElement(link.value, response.data.result.full_short_link2);
+        link.value = '';
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
